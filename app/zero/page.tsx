fc862b4824f742a8ed92fa883067206b228c690d
Ashish <ashishjadhav3300@gmail.com>
2024-08-06 23:08:33 +0530

    zero

---
+++ app/zero/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Zero = () => {
+  return (
+    <div className='text-24 font-extrabold absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]'>
+      Arriving Soon
+    </div>
+  )
+}
+
+export default Zero
